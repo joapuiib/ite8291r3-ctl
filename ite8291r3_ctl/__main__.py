45423730abe1a48d4fbfd78aef5fab7ed488e7e8
Joan Puigcerver <joapuiib@gmail.com>
2021-03-17 17:11:53 +0100

    improved readability

--- ite8291r3_ctl/__main__.py
+++ ite8291r3_ctl/__main__.py
@@ -300,75 +300,52 @@ def main():
                       f"product '{dev.product}' "
                       f"manufacturer '{dev.manufacturer}'")
 
-    parser = argparse.ArgumentParser(
-        description='ITE8291 (rev 0.03) RGB keyboard backlight controller driver.')
+    parser = argparse.ArgumentParser(description='ITE8291 (rev 0.03) RGB keyboard backlight controller driver.')
 
-    parser.add_argument('--device', type=valid_devid,
-                        help='bus/addr of the device to control.')
+    parser.add_argument('--device', type=valid_devid, help='bus/addr of the device to control.')
 
     subparsers = parser.add_subparsers(help='Subcommands.')
 
-    parser_off = subparsers.add_parser(
-        'off', help='Turn keyboard backlight off.')
+    parser_off = subparsers.add_parser('off', help='Turn keyboard backlight off.')
     parser_off.set_defaults(func=handle_off_args)
 
-    parser_brightness = subparsers.add_parser(
-        'brightness', help='Change keyboard backlight brightness.')
-    parser_brightness.add_argument('brightness', type=valid_intrange(
-        0, 50), help='Keyboard backlight brightness.')
+    parser_brightness = subparsers.add_parser('brightness', help='Change keyboard backlight brightness.')
+    parser_brightness.add_argument('brightness', type=valid_intrange(0, 50), help='Keyboard backlight brightness.')
     parser_brightness.set_defaults(func=handle_brightness_args)
 
-    parser_test_pattern = subparsers.add_parser(
-        'test-pattern', help='Show the test pattern.')
+    parser_test_pattern = subparsers.add_parser('test-pattern', help='Show the test pattern.')
     parser_test_pattern.set_defaults(func=handle_test_pattern_args)
 
-    parser_freeze = subparsers.add_parser(
-        'freeze', help='Stop the running animation. [possibly unsafe]')
+    parser_freeze = subparsers.add_parser('freeze', help='Stop the running animation. [possibly unsafe]')
     parser_freeze.set_defaults(func=handle_freeze_args)
 
-    parser_effect = subparsers.add_parser(
-        'effect', help='Change keyboard backlight effect.')
+    parser_effect = subparsers.add_parser('effect', help='Change keyboard backlight effect.')
     parser_effect.add_argument('effect_name', choices=ite8291r3.effects.keys())
-    parser_effect.add_argument(
-        '-s', '--speed', type=valid_intrange(0, 10), help='Speed of the effect.')
-    parser_effect.add_argument(
-        '-b', '--brightness', type=valid_intrange(0, 50), help='Brightness of the effect.')
-    parser_effect.add_argument(
-        '-c', '--color', choices=ite8291r3.colors.keys(), help='Color of the effect.')
-    parser_effect.add_argument('--save', action='store_true',
-                               help='Instruct the controller to save the settings.')
+    parser_effect.add_argument('-s', '--speed', type=valid_intrange(0, 10), help='Speed of the effect.')
+    parser_effect.add_argument('-b', '--brightness', type=valid_intrange(0, 50), help='Brightness of the effect.')
+    parser_effect.add_argument('-c', '--color', choices=ite8291r3.colors.keys(), help='Color of the effect.')
+    parser_effect.add_argument('--save', action='store_true', help='Instruct the controller to save the settings.')
     parser_effect.set_defaults(func=handle_effect_args)
 
     group = parser_effect.add_mutually_exclusive_group()
-    group.add_argument(
-        '-d', '--direction', choices=ite8291r3.directions.keys(), help='Direction of the effect.')
-    group.add_argument('-r', '--reactive', action='store_true',
-                       help='Specify if you want the effect to be reactive.')
+    group.add_argument('-d', '--direction', choices=ite8291r3.directions.keys(), help='Direction of the effect.')
+    group.add_argument('-r', '--reactive', action='store_true', help='Specify if you want the effect to be reactive.')
 
-    parser_monocolor = subparsers.add_parser(
-        'monocolor', help='Change keyboard backlight color.')
-    parser_monocolor.add_argument(
-        '-b', '--brightness', type=valid_intrange(0, 50), help='Brightness of the color.')
+    parser_monocolor = subparsers.add_parser('monocolor', help='Change keyboard backlight color.')
+    parser_monocolor.add_argument('-b', '--brightness', type=valid_intrange(0, 50), help='Brightness of the color.')
     group = parser_monocolor.add_mutually_exclusive_group()
-    group.add_argument('--name', choices=color_name_to_rgb.keys(),
-                       help='Specify color by name.')
-    group.add_argument('--rgb', type=valid_rgb,
-                       metavar='red,green,blue', help='Specify color by RGB code.')
+    group.add_argument('--name', choices=color_name_to_rgb.keys(), help='Specify color by name.')
+    group.add_argument('--rgb', type=valid_rgb, metavar='red,green,blue', help='Specify color by RGB code.')
     parser_monocolor.set_defaults(func=handle_monocolor_args)
 
-    parser_palette = subparsers.add_parser(
-        'palette', help='Change keyboard color palette.')
+    parser_palette = subparsers.add_parser('palette', help='Change keyboard color palette.')
     group = parser_palette.add_mutually_exclusive_group()
-    group.add_argument('--set-color', nargs=2,
-                       help='Change the given color of the palette to an arbitrary color.')
-    group.add_argument('--restore', action='store_true',
-                       help='Restore palette to default values.')
-    group.add_argument('--random', action='store_true',
-                       help='Set a random palette.')
+    group.add_argument('--set-color', nargs=2, help='Change the given color of the palette to an arbitrary color.')
+    group.add_argument('--restore', action='store_true', help='Restore palette to default values.')
+    group.add_argument('--random', action='store_true', help='Set a random palette.')
     parser_palette.set_defaults(func=handle_palette_args)
 
-    parser_mode = subparsers.add_parser(
-        'mode', help='Enable interactive modes.')
+    parser_mode = subparsers.add_parser('mode', help='Enable interactive modes.')
     group = parser_mode.add_mutually_exclusive_group()
     group.add_argument('--screen', metavar='offset_x,offset_y,width,height', nargs='?', const='fullscreen',
                        help='Color the keyboard according to what is on the screen in the given region.')
@@ -377,19 +354,15 @@ def main():
     parser_anim = subparsers.add_parser('anim', help='Play animation.')
     parser_anim.add_argument('--file', type=argparse.FileType('r', bufsize=1), default=sys.stdin,
                              help='Read animation from file. If not specified, stdin is used.')
-    parser_anim.add_argument('--loop', nargs='?', const=True, default=1, type=valid_intrange(
-        1), help='Repeat the animation this number of times. Omit the argument to repeat indefinitely.')
+    parser_anim.add_argument('--loop', nargs='?', const=True, default=1, type=valid_intrange(1),
+                             help='Repeat the animation this number of times. Omit the argument to repeat indefinitely.')
     parser_anim.set_defaults(func=handle_anim_args)
 
     parser_query = subparsers.add_parser('query', help='Query the controller.')
-    parser_query.add_argument(
-        '--fw-version', action='store_true', help='Get the controller firmware version.')
-    parser_query.add_argument(
-        '-b', '--brightness', action='store_true', help='Get the current brightness.')
-    parser_query.add_argument('--state', action='store_true',
-                              help='Get the current state of the keyboard backlight.')
-    parser_query.add_argument('--devices', action='store_true',
-                              help='List available devices that may be controlled.')
+    parser_query.add_argument('--fw-version', action='store_true', help='Get the controller firmware version.')
+    parser_query.add_argument('-b', '--brightness', action='store_true', help='Get the current brightness.')
+    parser_query.add_argument('--state', action='store_true', help='Get the current state of the keyboard backlight.')
+    parser_query.add_argument('--devices', action='store_true', help='List available devices that may be controlled.')
     parser_query.set_defaults(func=handle_query_args)
 
     argcomplete.autocomplete(parser)
